993233389550dcc3ab5300c2cc1da9807ace0ab5
yp6aHouD <danilnelubin@yandex.ru>
2024-01-20 11:46:48 +0800

    20.01.24 // added guess_dummies_frame, calc_dummies_frame

--- Main_file.java
+++ Main_file.java
@@ -1,17 +1,7 @@
-import com.allmycode.dummiesframe.DummiesFrame;
-
 public class Main_file
 {
     public static void main(String[] args)
     {
-        DummiesFrame frame = new DummiesFrame("Поздоровайтесь со мной, ссуки!");
-        frame.addRow("Укажите Ваше имя, ССУКА!");
-        frame.addRow("А ещё рост!");
-        frame.go();
-    }
 
-    public static String calculate(String firstName, int bodyHeight)
-    {
-        return "Привет, " + firstName + " с ростом " + bodyHeight + "!";
     }
 }
---
+++ use_dummies_frame.java
@@ -0,0 +1,17 @@
+import com.allmycode.dummiesframe.DummiesFrame;
+
+public class use_dummies_frame
+{
+    public static void main(String[] args)
+    {
+        DummiesFrame frame = new DummiesFrame("Поздоровайтесь со мной, ссуки!");
+        frame.addRow("Укажите Ваше имя, ССУКА!");
+        frame.addRow("А ещё рост!");
+        frame.go();
+    }
+
+    public static String calculate(String firstName, int bodyHeight)
+    {
+        return "Привет, " + firstName + " с ростом " + bodyHeight + "!";
+    }
+}
