a7fc35f3a97a6106b8e6b9a63412322799c7213d
yp6aHouD <danilnelubin@yandex.ru>
2024-02-03 01:09:00 +0800

    02.02.2024 // added SimpleHTTPServer.java

--- Main_file.java
+++ Main_file.java
@@ -1,14 +1,9 @@
-import static java.lang.System.out;
-import java.util.Locale;
-import java.util.Scanner;
-
 
 
 public class Main_file
 {
     public static void main(String[] args)
     {
-        Locale.setDefault(Locale.CHINA);
-
+        
     }
 }
---
+++ SimpleHTTPServer.java
@@ -0,0 +1,22 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class SimpleHTTPServer
+{
+    public static void main(String[] args) throws Exception
+    {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for the connection on port 8080...");
+        while (true)
+        {
+            try (Socket socket = server.accept())
+            {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream()
+                    .write(httpResponse.getBytes("UTF-8"));
+            }
+        }
+    }
+}
