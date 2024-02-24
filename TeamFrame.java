f18b9bbd40aff41cf156556e0d3f92e95698591e
yp6aHouD <danilnelubin@yandex.ru>
2024-02-24 21:08:12 +0800

    24.02.24 // added TeamFrame.java & UseTeamFrame.java

---
+++ TeamFrame.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+
+@SuppressWarnings("serial")
+public class TeamFrame extends JFrame 
+{
+    public TeamFrame() throws IOException
+    {
+        BaseballPlayer player;
+        Scanner hankeesData = new Scanner(new File("Hankees.txt"));
+
+        for (int num = 1; num <= 9; num++)
+        {
+            player = new BaseballPlayer(hankeesData.nextLine(), hankeesData.nextDouble());
+            hankeesData.nextLine();
+            addPlayerInfo(player);
+        }
+
+        setTitle("Average Hankees betting");
+        setLayout(new GridLayout(9, 2, 20, 3));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+
+        hankeesData.close();
+    }
+
+    void addPlayerInfo(BaseballPlayer player)
+    {
+        add (new JLabel(" " + player.getName()));
+        add (new JLabel(" " + player.getAverageBallsString()));
+    }
+}
---
+++ UseTeamFrame.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Locale;
+
+public class UseTeamFrame
+{
+    
+    public static void main(String[] args) throws IOException
+    {
+        Locale.setDefault(Locale.CHINA);
+        new TeamFrame();
+    }
+}
