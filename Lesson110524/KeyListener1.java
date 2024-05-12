fd096d4156ccda28e61191e4957032db8d84c14d
yp6aHouD <danilnelubin@yandex.ru>
2024-05-12 10:24:39 +0800

    12.05.24 // edited KeyListener1

--- Lesson110524/KeyListener1.java
+++ Lesson110524/KeyListener1.java
@@ -4,15 +4,14 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
+@SuppressWarnings("unused")
 class UseKeyListener1 implements KeyListener
 { //定义事件监听器类
 public void keyPressed(KeyEvent e)
 {
     System.out.println("getKeyChar()="+e.getKeyChar());
     System.out.println("getKeyCode()="+e.getKeyCode());
-    System.out.println("getKeyText()="+e.getKeyText(e.getKeyCode()));
-    System.out.println("getModifiers()="+e.getModifiers());
-    System.out.println("getKeyModifiersText()="+e.getKeyModifiersText(e.getModifiers()));
+    System.out.println("getKeyText()="+KeyEvent.getKeyText(e.getKeyCode()));
     System.out.println("isActionKey()="+e.isActionKey());
     System.out.println("getID()="+e.getID());
     System.out.println("getWhen()="+e.getWhen());
@@ -24,6 +23,7 @@ public void keyPressed(KeyEvent e)
     public void keyReleased(KeyEvent e){}
     public void keyTyped(KeyEvent e){}
 }
+
 class FrameForKeyListener1
 {
     public FrameForKeyListener1()
@@ -34,6 +34,8 @@ class FrameForKeyListener1
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
+
+
 public class KeyListener1
 {
     public static void main(String[] args)
